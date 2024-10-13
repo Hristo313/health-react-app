2b420baab6488bff4443cbe635c7693b0d51d139
Hristo Hristov <HHristov@infragistics.com>
2024-10-13 15:34:19 +0300

    remove console log

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { routes } from "./app/app-routes";
 import 'react-app-polyfill/ie11';
 
 const basename = import.meta.env.VITE_BASENAME || '/';
-console.log("Basename: ", import.meta.env.VITE_BASENAME)
+
 /** Required in IE11 for Charts */
 Number.isNaN = Number.isNaN || function(value) {
   return value !== value;
